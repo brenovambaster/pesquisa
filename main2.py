21fc34c629feeef22cd4b691cff72f4ec10e8a33
Breno Vambaster <brenovambaster5@gmail.com>
2024-05-31 15:35:09 -0300

    update: descriptor

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ from classes import htd, cld, csd, distances, descriptor
 image1 = cv2.imread("images/test/6.jpg")
 image2 = cv2.imread("images/test/15.jpg")
 htd_instance = htd.HTD()
-p2= htd_instance.extract_features(image2)
+p2 = htd_instance.extract_features(image2)
 
 descriptor_instance = descriptor.Descriptor(image1, "HTD", "euclidean", p2)
 
