0048d1513ba662fbc0397c79e52e1411d78790ca
Breno Vambaster <bvcl@aluno.ifnmg.edu.br>
2024-06-11 09:33:28 -0300

    outra implementação para o extrator cld

--- classes/cld.py
+++ classes/cld.py
@@ -1,45 +1,94 @@
 import cv2
 import numpy as np
-from .interfaces.IExtractor import IExtractor
+from scipy.fftpack import dct
 
+class CLD:
+    def __init__(self, num_y_coeffs=6, num_cb_coeffs=3, num_cr_coeffs=3):
+        """
 
-class CLD(IExtractor):
-    """
-    Color Layout Descriptor (CLD)
-    """
-
-    def __init__(self, num_blocks=8, num_bins=32):
-        self.num_blocks = num_blocks
-        self.num_bins = num_bins
+        :param num_y_coeffs: Number of Y channel Discrete Cosine Transform coefficients to keep.
+        :param num_cb_coeffs: Number of Cb channel Discrete Cosine Transform coefficients to keep.
+        :param num_cr_coeffs: Number of Cr channel Discrete Cosine Transform coefficients to keep.
+        """
+        self.num_y_coeffs = num_y_coeffs
+        self.num_cb_coeffs = num_cb_coeffs
+        self.num_cr_coeffs = num_cr_coeffs
 
     def extract_features(self, image):
         """
-        Extracts the Color Layout Descriptor (CLD) features from an image.
-        :param image:
-        :return cld_vector:
+        Extract the Color Layout Descriptor (CLD) features from an image.
+        :param image: The image from which to extract the features.
+        :return: A list of the extracted features.
+        :rtype: numpy.array
         """
+        # Convert the image from RGB to YCbCr
+        ycbcr_image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
 
-        # Converter a imagem para o espaço de cor HSV
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        height, width, _ = hsv_image.shape
+        # Split the image into Y, Cb, and Cr channels
+        y_channel, cb_channel, cr_channel = cv2.split(ycbcr_image)
 
-        cld_vector = []
+        # Resize the channels to 8x8
+        y_channel_resized = cv2.resize(y_channel, (8, 8))
+        cb_channel_resized = cv2.resize(cb_channel, (8, 8))
+        cr_channel_resized = cv2.resize(cr_channel, (8, 8))
 
-        # Dividir a imagem em blocos
-        block_height = height // self.num_blocks
-        block_width = width // self.num_blocks
+        # Apply DCT to each channel
+        y_dct = dct(dct(y_channel_resized.T, norm='ortho').T, norm='ortho')
+        cb_dct = dct(dct(cb_channel_resized.T, norm='ortho').T, norm='ortho')
+        cr_dct = dct(dct(cr_channel_resized.T, norm='ortho').T, norm='ortho')
 
-        for i in range(self.num_blocks):
-            for j in range(self.num_blocks):
-                # Obter o bloco atual
-                block = hsv_image[i * block_height:(i + 1) * block_height,
-                        j * block_width:(j + 1) * block_width]
+        # Collect the DCT coefficients
+        y_coeffs = self._collect_coeffs(y_dct, self.num_y_coeffs)
+        cb_coeffs = self._collect_coeffs(cb_dct, self.num_cb_coeffs)
+        cr_coeffs = self._collect_coeffs(cr_dct, self.num_cr_coeffs)
 
-                # Calcular o histograma de matiz do bloco
-                hist = cv2.calcHist([block], [0], None, [self.num_bins], [0, 256])
-                hist = cv2.normalize(hist, hist).flatten()
+        # Normalize the coefficients
+        y_coeffs = y_coeffs / np.linalg.norm(y_coeffs)
+        cb_coeffs = cb_coeffs / np.linalg.norm(cb_coeffs)
+        cr_coeffs = cr_coeffs / np.linalg.norm(cr_coeffs)
 
-                # Adicionar o histograma ao vetor CLD
-                cld_vector.extend(hist)
+        # Combine the coefficients into a single feature vector
+        feature_vector = np.concatenate([y_coeffs, cb_coeffs, cr_coeffs])
 
-        return np.array(cld_vector)
+        return feature_vector.tolist()
+
+    def _collect_coeffs(self, dct_block, num_coeffs):
+        """
+        This method collects the DCT coefficients from a given DCT block.
+        It follows a zigzag order to collect the coefficients, which is a common practice in image processing tasks
+        when working with DCT transformed blocks. The number of coefficients to collect is determined by the 'num_coeffs' parameter.
+
+        :param dct_block: 2D array representing the DCT transformed block from which to collect the coefficients.
+        :type dct_block: numpy.ndarray
+
+        :param num_coeffs: The number of coefficients to collect from the DCT block.
+        :type num_coeffs: int
+
+        :return: A 1D array of the collected coefficients.
+        :rtype: numpy.ndarray
+        """
+        zigzag_order = [
+            (0,0), (0,1), (1,0), (2,0), (1,1), (0,2), (0,3), (1,2),
+            (2,1), (3,0), (4,0), (3,1), (2,2), (1,3), (0,4), (0,5),
+            (1,4), (2,3), (3,2), (4,1), (5,0), (6,0), (5,1), (4,2),
+            (3,3), (2,4), (1,5), (0,6), (0,7), (1,6), (2,5), (3,4),
+            (4,3), (5,2), (6,1), (7,0), (7,1), (6,2), (5,3), (4,4),
+            (3,5), (2,6), (1,7), (2,7), (3,6), (4,5), (5,4), (6,3),
+            (7,2), (7,3), (6,4), (5,5), (4,6), (3,7), (4,7), (5,6),
+            (6,5), (7,4), (7,5), (6,6), (5,7), (6,7), (7,6), (7,7)
+        ]
+        coeffs = []
+        for idx in range(num_coeffs):
+            x, y = zigzag_order[idx]
+            coeffs.append(dct_block[x, y])
+        return np.array(coeffs)
+
+    def compare(self, feat1, feat2):
+        """
+        Compute the similarity between two feature vectors.
+        :param feat1:
+        :param feat2:
+        :return:
+        """
+        similarity = np.linalg.norm(np.array(feat1) - np.array(feat2))
+        return similarity
---
+++ mainCLD.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+
+from classes.descriptor import Descriptor
+from classes.ImageReader import ImageReader
+from classes.extract_info_file import FileProcessor
+
+# Read the image
+image1 = ImageReader("base_imgs_testes/61.jpg").read_image()
+
+# Extract features from the image
+htd_obj = Descriptor(image1, 'CLD')
+feat_1 = htd_obj.extractor.extract_features(image1)
+
+# Read the database file and recuperate the tuples
+file_processor = FileProcessor('output/databaseCLD.txt')
+data = file_processor.process_file()
+
+# Transform the features of the image to a numpy array float32
+h1 = np.array(feat_1, dtype=np.float32)
+
+"""
+TODO: Compare the features of the image with the features of the all images in the database
+NOTA:  Ajuste provisório para que encontre as imagens próximas, essa busca deve ser de responsabilidade da classe 
+`operador de busca`
+
+Atencao: Em alguns casos,  esse trecho não mostra a imagem mais próxima de fato, mas sim outras imagens que não 
+são tão próximas 
+"""
+distances = []
+for i in data:
+    h2 = np.array(i['features'], dtype=np.float32)
+    distance_info = {
+        'id': i['id'],
+        'distance': float(htd_obj.extractor.compare(h1, h2)),
+        'path_img': i['path']
+    }
+    distances.append(distance_info)
+
+# Sort the distances and return the ids of the most similar images
+distances = sorted(distances, key=lambda item: item['distance'])
+
+# Print the 5 most similar images
+print(distances[:5])
+
+
+# Initialize an empty list to store the images
+images = []
+
+# Loop over the paths of the 5 most similar images
+for i in distances[:5]:
+    path = i['path_img'].replace("../", "")
+    # Read the image
+    image = cv2.imread(path)
+    image = cv2.resize(image, (400, 300))
+
+    # Add a 3px padding to the image
+    image = cv2.copyMakeBorder(image, 3, 3, 3, 3, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+
+    # Append the image to the list
+    images.append(image)
+
+# Concatenate the images horizontally
+concatenated_image = np.hstack(images)
+# Display the concatenated image
+cv2.imshow("Images", concatenated_image)
+cv2.waitKey(0)
+
