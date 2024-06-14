f6a4e9e6bcc52d8d5b90a983bd4bd8038f5cd9bf
Breno Vambaster <bvcl@aluno.ifnmg.edu.br>
2024-06-14 18:25:27 -0300

    1

---
+++ scripts/extrair_images_dir.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+# Função para extrair imagens de diretórios de 1 a 15 e copiá-las para o diretório raiz
+
+
+def copiar_imagens(root_dir):
+    # Percorrer todos os diretórios de 1 a 100
+    for i in range(1, 201):
+        # Caminho do diretório atual
+        current_dir = os.path.join(root_dir, str(i))
+
+        # Verifica se o diretório existe
+        if os.path.exists(current_dir) and os.path.isdir(current_dir):
+            # Lista todos os arquivos no diretório atual
+            arquivos = os.listdir(current_dir)
+
+            # Filtra apenas arquivos de imagem (extensões comuns)
+            imagens = [f for f in arquivos if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff'))]
+
+            # Copia cada imagem para o diretório raiz
+            counter = 0
+            for img in imagens:
+                if counter == 20:
+                    break
+                source = os.path.join(current_dir, img)
+                destination = os.path.join(root_dir, img)
+
+                # Verifica se o arquivo já existe no destino para evitar sobreposição
+                if not os.path.exists(destination):
+                    shutil.copy(source, destination)
+                    counter+= 1
+                else:
+                    print(f"Arquivo {img} já existe no destino. Ignorando...")
+
+# Chama a função
+copiar_imagens('../base_imgs_testes/')
---
+++ scripts/extrair_precisao_revocacao.py
@@ -0,0 +1,24 @@
+class extrai_precisao_revocao:
+
+    def compute(self, array, classe, num_imgs_por_classe=20):
+        """
+
+        :param array: Saída da função all_knn, apenas o caminho das imagens
+        :param classe: classe da imagem que está sendo analisada
+        :param num_imgs_por_classe: Quantidade de imagens que cada classe na base
+        :return:
+        """
+        size_of_array = len(array)
+        count = 0
+        recall = []
+        precision = []
+
+        for i in array:
+            nome_img = i.replace('base_imgs_testes/', '').replace('../', '').split('_')[0]
+            if nome_img == classe:
+                count += 1
+
+            precision.append(float(format(count / (array.index(i) + 1), '.3f')))
+            recall.append(float(format(count / num_imgs_por_classe, '.3f')))
+
+        return precision, recall
