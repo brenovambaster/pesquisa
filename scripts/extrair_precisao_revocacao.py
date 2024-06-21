bcbb36b92757e543179e6b403ae86fb29591ac19
Breno Vambaster <brenovambaster5@gmail.com>
2024-06-21 14:58:40 -0300

    corrige cálculo do recall

--- scripts/extrair_precisao_revocacao.py
+++ scripts/extrair_precisao_revocacao.py
@@ -1,5 +1,5 @@
 class ExtraiPrecisaoRevocao:
-
+    TAMANHO_DA_BASE = 4000
     def compute(self, array, classe, num_imgs_por_classe=20, dir_base_imgs='base_imgs_testes/'):
         """
 
@@ -20,6 +20,6 @@ class ExtraiPrecisaoRevocao:
                 count += 1
 
             precision.append(float(format(count / (array.index(i) + 1), '.3f')))
-            recall.append(float(format(count / num_imgs_por_classe, '.3f')))
+            recall.append(float(format(count / self.TAMANHO_DA_BASE, '.3f')))
 
         return precision, recall
