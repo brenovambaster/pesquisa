89fc878e6a85ebe54659988d6764d10cae356692
Breno Vambaster <bvcl@aluno.ifnmg.edu.br>
2025-04-15 15:33:30 -0300

    fix: recompila base de dados

--- mainHTD.py
+++ mainHTD.py
@@ -4,7 +4,7 @@ from scripts.show_images import ShowImages
 from classes.OperadorDeBusca import SearchOperator
 
 # Read the image
-image1 = ImageReader("base_imgs_teste_query/5_r0.png").read_image()
+image1 = ImageReader("base_imgs_teste_query/1_r0.png").read_image()
 
 operador_de_busca = SearchOperator('output/databaseHTD.txt')
 list_similar_imgs = operador_de_busca.all_knn(image1, 'HTD', k=20, distance_name=SearchOperator.EUCLIDEAN)
--- mainLBP.py
+++ mainLBP.py
@@ -4,7 +4,7 @@ from scripts.show_images import ShowImages
 from classes.OperadorDeBusca import SearchOperator
 
 # Read the image
-image1 = ImageReader("query/31.jpg").read_image()
+image1 = ImageReader("base_imgs_teste_query/1_r0.png").read_image()
 
 operador_de_busca = SearchOperator('output/databaseLBP.txt')
 list_similar_imgs = operador_de_busca.all_knn(image1, 'LBP', k=9, distance_name=SearchOperator.EUCLIDEAN)
@@ -12,5 +12,5 @@ list_similar_imgs = operador_de_busca.all_knn(image1, 'LBP', k=9, distance_name=
 for obj in list_similar_imgs:
     print(obj['path_img'])
 
-ShowImages("database").show(list_similar_imgs)
+ShowImages("base_imgs_testes").show(list_similar_imgs)
 
